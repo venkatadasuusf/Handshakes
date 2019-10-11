5a94bb9cb89a62c23b791de8876b32c5a72251d5
Venkata Dasu <vdasu@mail.usf.edu>
2019-10-11 09:33:52 -0400

    Print line

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace Handshakes
             int number_students = 30;
             int total_handshakes = 0;
 
+            Console.Write("Total Number of Handshakes between " + number_students + " individuals: ");
+
             while (number_students > 0)
             {
                 number_students--;
