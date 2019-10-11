822a5c798b7b0098c05a42adbbbdced31b796b0b
Venkata Dasu <vdasu@mail.usf.edu>
2019-10-10 20:14:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Handshakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_students = 30;
+            int total_handshakes = 0;
+
+            while (number_students > 0)
+            {
+                number_students--;
+                total_handshakes = total_handshakes + number_students;
+
+            }
+
+            Console.WriteLine(total_handshakes);
+        }
+    }
+}
